826e76f05f1c6deb033b538d726d1eb382059355
ry-Rahul <ryrahul12345@gmail.com>
2024-04-16 12:24:48 +0530

    updated

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,6 +1,13 @@
+import Product from "../Products/Product/Product";
+import Products from "../Products/Products";
 import "./Category.scss";
 const Category = () => {
-    return <div>Category</div>;
+    return <div className="category-main-content">
+        <div className="layout">
+            <div className="category-title">Title</div>
+            <Products innerPage={true} />
+        </div>
+    </div>;
 };
 
 export default Category;
--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,6 +1,21 @@
 import "./Product.scss";
+import prod from "../../../assets/products/earbuds-prod-1.webp"
+import prod2 from "../../../assets/products/earbuds-prod-2.webp"
+import prod3 from "../../../assets/products/earbuds-prod-3.webp"
+import prod4 from "../../../assets/products/earbuds-prod-4.webp"
+import prod5 from "../../../assets/products/earbuds-prod-5.webp"
 const Product = () => {
-    return <div>Product</div>;
+  return (
+    <div className="product-card">
+      <div className="thumbnail">
+        <img src={prod} alt="" />
+      </div>
+      <div className="prod-details">
+        <span className="name">Product name</span>
+        <span className="price">&#8377;499</span>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
